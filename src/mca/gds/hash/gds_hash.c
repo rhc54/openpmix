d35271e1757563e5e03fe26da9ece67b2cefc7e4
Ralph Castain <rhc@pmix.org>
2024-03-16 06:15:58 -0600

    Checkpoint - more work on group modex data return
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    checkpoint again
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    ckpt again
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    ckpt
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    ckpt
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    Complete the PMIx_Connect support
    
    Have each client includes its PMIX_GLOBAL information
    that it "put" - we use global in case the connect
    operation involves multiple nspaces and local connection
    between them is allowed.
    
    Add a couple of new "info_list" functions to add a pmix_value_t
    to the list, and to do so uniquely.
    
    Add a missing function to load a byte object - was in the
    headers but missing from implementation. Not sure how that
    wasn't being caught.
    
    Refactor the connect and fence operations for both client
    and server into their own files.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    Get simptest/group to work
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    Get group example working`
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    ckpt
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    ckpt - group example works
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    ckpt - grp info example complete
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    
    ckpt
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/mca/gds/hash/gds_hash.c
+++ src/mca/gds/hash/gds_hash.c
@@ -1435,7 +1435,8 @@ static pmix_status_t nspace_del(const char *nspace)
     return PMIX_SUCCESS;
 }
 
-static pmix_status_t assemb_kvs_req(const pmix_proc_t *proc, pmix_list_t *kvs, pmix_buffer_t *buf,
+static pmix_status_t assemb_kvs_req(const pmix_proc_t *proc,
+                                    pmix_list_t *kvs, pmix_buffer_t *buf,
                                     void *cbdata)
 {
     pmix_status_t rc = PMIX_SUCCESS;
